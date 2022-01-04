07b9a6321ef3cb45586f58f65e572567b03ee422
YooKyeong <rladbrud1936@gmail.com>
2022-01-04 23:46:04 +0900

    2805

---
+++ Python/2805.py
@@ -0,0 +1,22 @@
+import sys
+
+N, M = map(int,input().split())
+
+N_list = list(map(int,sys.stdin.readline().split()))
+
+start, end = 0, max(N_list)
+
+while start <= end:
+    mid = (start+end) // 2
+
+    sum = 0
+    for i in N_list:
+        if i > mid:
+            sum += (i - mid)
+            if sum > M:
+                break
+    if sum >= M:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
