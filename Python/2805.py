4da61803fca30f50701471fda6a7509d7684602a
MilkyWay-U <rladbrud0302@naver.com>
2022-01-05 00:00:46 +0900

    2805_2

--- Python/2805.py
+++ Python/2805.py
@@ -4,11 +4,10 @@ N, M = map(int,input().split())
 
 N_list = list(map(int,sys.stdin.readline().split()))
 
+# 이분 탐색 사용
 start, end = 0, max(N_list)
-
 while start <= end:
     mid = (start+end) // 2
-
     sum = 0
     for i in N_list:
         if i > mid:
