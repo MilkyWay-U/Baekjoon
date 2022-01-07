fa5f3bdc3af6b12d3ff54346a27c50eed3ed0e26
MilkyWay-U <rladbrud0302@naver.com>
2022-01-07 23:56:19 +0900

    9996_finish

--- Python/9996.py
+++ Python/9996.py
@@ -1,38 +1,12 @@
-
 import sys
-sys.setrecursionlimit(10000)
-N = int(sys.stdin.readline())
-pattern = input()
-cnt = 0
-for i in pattern:
-    if i != '*':
-        cnt += 1
-    else:
-        break
-first = []
-for i in range(cnt):
-    first.append(pattern[i])
-cnt = 0
-for i in range(len(pattern)-1, 0, -1):
-    if pattern[i] != '*':
-        cnt +=1
-    else:
-        break
-end = []
-for i in range(cnt):
-    end.append(pattern[len(pattern)-1 -i])
-for i in range(N):
-    a = sys.stdin.readline()
-    result = 1
-    for i in range(len(first)):
-        if a[i] != first[i]:
-            result = 0
-    if result == 1:
-        for i in range(len(end)):
-            if a[len(a)-2-i] != end[i]:
-                result = 0
 
-    if result == 1:
+N = int(sys.stdin.readline())
+pattern = input().split('*')
+front = pattern[0]
+back = pattern[1]
+for i in range(N):
+    a = input()
+    if a[:len(front)] == front and a[-len(back):] == back and len("".join(pattern)) <= len(a):
         print("DA")
     else:
         print("NE")
