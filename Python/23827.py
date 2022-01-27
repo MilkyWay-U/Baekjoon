358263c949d5bc4e9f1eb4d8826bc970235575f9
MilkyWay-U <rladbrud0302@naver.com>
2022-01-27 22:35:59 +0900

    23827

--- Python/23827.py
+++ Python/23827.py
@@ -1,3 +1,10 @@
-# 1 000 000 007(02) => 256
-
+import sys
 num = int(input())
+num_list = list(map(int,sys.stdin.readline().split()))
+res = 0
+sum = sum(num_list)
+for i in num_list:
+    sum -= i
+    res += (i * sum)
+res = res % 1000000007
+print(res)
