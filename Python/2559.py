917e5c0366736072e45d3f9bac11d29695064170
MilkyWay-U <rladbrud0302@naver.com>
2022-03-24 17:19:08 +0900

    2559_2

--- Python/2559.py
+++ Python/2559.py
@@ -3,13 +3,14 @@ import sys
 N, K = map(int,sys.stdin.readline().split())
 NumList = list(map(int,sys.stdin.readline().split()))
 n = 0
-MaxVal = NumList[0] + NumList[K-1]
-print(MaxVal)
-
-while (n + K - 1 < N):
-    AddVal = 0
-    for i in range(K):
-        AddVal += NumList[n+i]
+AddVal = 0
+for i in range(K):
+    AddVal += NumList[n+i]
+MaxVal = AddVal
+print("first MaxVal : ",MaxVal)
+while (n + K < N):
+    AddVal -= NumList[n]
+    AddVal += NumList[n + K]
     if MaxVal < AddVal:
         MaxVal = AddVal
     n += 1
