bd80480254fa1e7f2f511daf0eb700e8853905b1
MilkyWay-U <rladbrud0302@naver.com>
2022-03-27 17:23:24 +0900

    4375_2

--- Python/4375.py
+++ Python/4375.py
@@ -1,10 +1,11 @@
 def div(num):
-    temp = 1
+    temp = '1'
     result = 1
-    while temp % num != 0:
-        temp *= 10
-        temp += 1
-        result +=1
+    while True:
+        if int(temp) % num == 0:
+            result = len(temp)
+            break
+        temp += '1'
     return result
 
 print(div(int(input())))
