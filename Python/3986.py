c00fd3df3a2b8012a32d368e1c4277f966ede59a
MilkyWay-U <rladbrud0302@naver.com>
2022-03-30 00:53:33 +0900

    3986_1

--- Python/3986.py
+++ Python/3986.py
@@ -1,18 +1,17 @@
 import sys
 input = sys.stdin.readline
-num = int(input())
- # 아직 다 못품;;; 킹받네;;;
-cnt = 0
-for i in range(num):
-    word = input().rstrip()
-    word_stack = []
-    for j in range(len(word)):
-        if word_stack and word[j] == word_stack[-1]:
-            word_stack.pop()
-        else:
-            word_stack.append(word[j])
-    
-    if not word_stack:
-        cnt += 1
-print(cnt)
 
+n = int(input())
+result = 0
+
+for i in range(n):
+    s = input().strip()
+    s_stack = []
+    for j in s:
+        if s_stack and s_stack[-1] == j:
+                s_stack.pop()
+        else:
+            s_stack.append(j)
+    if not s_stack:
+        result += 1
+print(result)
