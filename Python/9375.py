9ecd40fbe0f92c73baa4f23abbac755868978e28
MilkyWay-U <rladbrud0302@naver.com>
2022-03-30 00:25:06 +0900

    9375_1

--- Python/9375.py
+++ Python/9375.py
@@ -1,21 +1,14 @@
-# 공부할 것.
+from collections import Counter
 
-
-
-names = input()
-name_cnt = [0 for _ in range(26)]
-for name in names:
-    name_cnt[ord(name)-65] += 1
-odd = 0
-odd_alpha = ''
-alpha = ''
-for i in range(26):
-    if name_cnt[i] % 2 == 1:
-        odd += 1
-        odd_alpha += chr(i+65)
-    alpha += chr(i+65) * (name_cnt[i]// 2)
-
-if odd > 1:
-    print("I'm Sorry Hansoo")
-else:
-    print(alpha+odd_alpha+alpha[::-1])
+n = int(input())
+for i in range(n):
+    m = int(input())
+    wear = []
+    for j in range(m):
+        a, b = input().split()
+        wear.append(wear)
+    num = 1
+    wear_count = Counter(wear)
+    for k in wear_count:
+        num *= wear_count[k] + 1
+    print(num - 1)
