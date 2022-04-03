3839d3bdf995679e5d39ea5b2d02872e0e6b7a2f
MilkyWay-U <rladbrud0302@naver.com>
2022-04-04 02:03:00 +0900

    15649_1

---
+++ Python/15649.py
@@ -0,0 +1,16 @@
+N, M = map(input().split())
+
+lst = []
+
+def dfs():
+    if len(lst) == M:
+        print(' '.join(map(str,lst)))
+        return
+
+    for i in range(1, N + 1):
+        if i not in lst:
+            lst.append(i)
+            dfs()
+            lst.pop()
+
+dfs()
