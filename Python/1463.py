97051a5e9c0f9dcf39677728b1e01680a5c6be03
MilkyWay-U <rladbrud0302@naver.com>
2022-04-03 18:55:55 +0900

    1463_2_err

--- Python/1463.py
+++ Python/1463.py
@@ -2,13 +2,13 @@ N = int(input())
 num = 1
 cnt = 0
 while (N != num):
-    if (N >= (num * 3)):
+    while (N >= (num * 3)):
         num *= 3
         cnt += 1
-    elif ( N >= (num *2)):
+    while ( N >= (num *2)):
         num *= 2
         cnt += 1
-    else:
+    while ( N >= (num+1)):
         num += 1        
         cnt += 1
 print(cnt)
