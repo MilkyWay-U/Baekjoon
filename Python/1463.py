c52d269b3896747ff151c8a1ee797908c1425119
MilkyWay-U <rladbrud0302@naver.com>
2022-04-03 22:38:11 +0900

    1463_3

--- Python/1463.py
+++ Python/1463.py
@@ -1,14 +1,14 @@
-N = int(input())
-num = 1
-cnt = 0
-while (N != num):
-    while (N >= (num * 3)):
-        num *= 3
-        cnt += 1
-    while ( N >= (num *2)):
-        num *= 2
-        cnt += 1
-    while ( N >= (num+1)):
-        num += 1        
-        cnt += 1
-print(cnt)
+##이해못해못해ㅣ....
+
+
+n = int(input())
+d = [0] * (n + 1)	
+
+for i in range(2, n + 1):
+
+    d[i] = d[i - 1] + 1
+    if i % 3 == 0 and d[i] > d[i//3] + 1:
+        d[i] = d[i // 3] + 1
+    if i % 2 == 0 and d[i] > d[i // 2] + 1:
+        d[i] = d[i // 2] + 1
+print(d[n])
