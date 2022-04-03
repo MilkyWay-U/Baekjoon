6f95bdbdfa08b92c1f1895cbaee22f343b408f79
MilkyWay-U <rladbrud0302@naver.com>
2022-04-03 18:33:38 +0900

    10814_2

--- Python/10814.py
+++ Python/10814.py
@@ -1,7 +1,9 @@
 N = int(input())
 lst = []
 for _ in range(N):
-    lst.append(input().split())
-lst.sort(key = lambda x : int(x[0]))
-for i in range(N):
-    print(lst[i][0], lst[i][1])
+    age, name = map(str, input().split())
+    age = int(age)
+    lst.append((age,name))
+lst.sort(key = lambda x : x[0])
+for i in lst:
+    print(i[0], i[1])
