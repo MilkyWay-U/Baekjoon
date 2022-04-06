4f593c9b9e3cab104f2654d6e4fae073b6fb216b
MilkyWay-U <rladbrud0302@naver.com>
2022-04-06 21:07:55 +0900

    11723_1

--- Python/11723.py
+++ Python/11723.py
@@ -1,28 +1,29 @@
 import sys
-
+input = sys.stdin.readline
 m = int(input())
-s = []
+s = set()
 for _ in range(m):
-    temp = sys.stdin.readline().strip().split()
-
+    temp = input().strip().split()
     if len(temp) == 1:
         s.clear()
         if temp[0] == "all":
-            for i in range(1,21):
-                s.append(i)
+            s = set([i for i in range(1,21)])
+        else:
+            s = set()
         continue
+
     a, b = temp[0],temp[1]
     b = int(b)
     if a == "add":
         if b not in s:
-            s.append(b)
+            s.add(b)
     elif a == "remove":
         if b in s:
-            s.remove(b)
+            s.discard(b)
     elif a == "check":
         print(1 if b in s else 0)
     elif a == "toggle":
         if b in s:
-            s.remove(b)
+            s.discard(b)
         else:
-            s.append(b)
+            s.add(b)
