35e3afcd47a96aa93d6aca30705f5a3d6d857c2f
MilkyWay-U <rladbrud0302@naver.com>
2022-04-07 02:15:43 +0900

    1654_1

---
+++ Python/1654.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+k, n = map(int,input().split())
+lst = [int(input()) for _ in range(k)]
+start, end = 1, max(lst)
+
+while start <= end:
+    mid = (start + end) // 2
+    cnt = 0
+    for i in lst:
+        cnt += i // mid
+
+    if cnt >= n:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
+
