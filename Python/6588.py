5c3d4db6c0f334e65e3da0598baaf6b1bd6101a8
MilkyWay-U <rladbrud0302@naver.com>
2022-04-06 18:30:45 +0900

    6588_1

---
+++ Python/6588.py
@@ -0,0 +1,24 @@
+import sys
+
+num = 1000001
+arr = [True for _ in range(num)]
+
+for i in range(2, 1001):
+    if arr[i]:
+        for j in range(2*i,num, i):
+            arr[j] = False
+
+while True:
+    n = int(sys.stdin.readline())
+    if n == 0:
+        break
+    flag = 0
+    
+    for k in range(3, n):
+        if arr[k] and arr[n-k]:
+            print(str(n)+"="+str(k)+"+"+str(n-k))
+            flag = 1
+            break
+    if flag == 0:
+        print("Goldbach's conjecture is wrong.")
+    
