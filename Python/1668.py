0f065976fe0cd1e6271e0157b26fc32659b4dc1c
MilkyWay-U <rladbrud0302@naver.com>
2022-04-07 00:43:00 +0900

    1668_2

--- Python/1668.py
+++ Python/1668.py
@@ -1,22 +1,20 @@
+import sys
+input = sys.stdin.readline
 n = int(input())
 lst = []
-max = 0
-l_cnt = 0
-r_cnt = 0
 for _ in range(n):
     lst.append(int(input()))
 
-for i in lst:
-    if (max < i):
-        max = i
-        l_cnt += 1
+def check(lst):
+    max = lst[0]
+    cnt = 1
+
+    for i in lst:
+        if (max < i):
+            max = i
+            cnt += 1
+    return cnt
+print(check(lst))
 lst = reversed(lst)
 lst = list(lst)
-max = 0
-for i in lst:
-    if (max < i):
-        max = i
-        r_cnt += 1
-
-print(l_cnt)
-print(r_cnt)
+print(check(lst))
