3788d85e1764d5dbd5c315f16711b149aab5c03a
MilkyWay-U <rladbrud0302@naver.com>
2022-04-14 17:04:46 +0900

    2530_plus

--- Python/2530.py
+++ Python/2530.py
@@ -1,17 +1,4 @@
 h, m, s = map(int,input().split())
 a = int(input())
-
-s += a % 60
-a = a// 60
-if s > 59:
-    s -= 60
-    m += 1
-m += a % 60
-a = a // 60
-if m > 59:
-    m -= 60
-    h += 1
-h += a % 24
-if h > 23:
-    h -= 24
-print(h,m,s)
+s += a
+print((h+((m + (s // 60)) // 60)) % 24,(m + (s // 60)) % 60,s % 60)
