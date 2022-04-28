d14bac3d90c0c058543e2d90e39e757c93c4cbbe
MilkyWay-U <rladbrud0302@naver.com>
2022-04-28 19:07:01 +0900

    2981_2

--- Python/2981.py
+++ Python/2981.py
@@ -1,19 +1,28 @@
-import sys
-input = sys.stdin.readline
-def solve(temp, N_list, a):
-    for i in N_list:
-        if temp != i % a:
-            return 1
-    return 0
-
 N = int(input())
-N_list = []
-for _ in range(N):
-    N_list.append(int(input()))
-result = []
-for i in range(2,int(max(N_list)**(1/2))+2):
-    temp = N_list[0] % i
-    if solve(temp,N_list, i) == 0:
-        result.append(i)
-for k in result:
-    print(k, end = ' ')
+num = sorted([int(input()) for _ in range(N)])
+re_num = []
+
+for i in range(1, N):
+    re_num.append(num[i] - num[i-1])
+
+def findGCD(a, b):
+    num = b
+    div = a
+    rest = num % div
+    while rest != 0:
+        num = div
+        div = rest
+        rest = num % div
+    return div
+
+GCD = re_num[0]
+for idx in range(1, len(re_num)):
+    GCD = findGCD(GCD, re_num[idx])
+
+result = set()
+for i in range(2, int(GCD**0.5) + 1):
+    if GCD % i == 0:
+        result.add(i)
+        result.add(GCD // i)
+result.add(GCD)
+print(*sorted(list(result)))
