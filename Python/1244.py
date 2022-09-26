8cfb82efccb66fe3c931163feb17a874b104d5c4
MilkyWay-U <rladbrud0302@naver.com>
2022-09-26 22:30:53 +0900

    1244_ing

---
+++ Python/1244.py
@@ -0,0 +1,30 @@
+def s2(lst, n, j, lst_len):
+    if ((n - j) >= 0 and (n + j) < lst_len):
+        if (lst[n - j] == lst[n + j]):
+            s2(lst, n , j + 1, lst_len)
+        return (j - 1)
+    return (j - 1)
+
+num = int(input())
+lst = list(map(int, input().split()))
+cnt = int(input())
+lst_len = len(lst)
+for i in range(cnt):
+    s, n = map(int,input().split())
+    n -= 1
+
+    if s == 1:
+        while (n < lst_len):
+            if lst[n] == 1:
+                lst[n] = 0
+            else:
+                lst[n] = 1
+            n += (n + 1)
+    else:
+        j = s2(lst, n, 1, lst_len)
+        for i in range(n - j, n + j):
+            if lst[i] == 1:
+                lst[i] = 0
+            else:
+                lst[i] = 1
+    print(*lst)
