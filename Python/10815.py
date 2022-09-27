5cc8e589b2cd87da7a9bc8f76e20b8fa9a3443bc
yk <rladbrud0302@naver.com>
2022-09-28 01:12:57 +0900

    10815_bin

--- Python/10815.py
+++ Python/10815.py
@@ -4,10 +4,23 @@ n = int(input())
 n_lst = list(map(int,input().split()))
 m = int(input())
 m_lst = list(map(int,input().split()))
-cnt_lst = [0] * len(m_lst)
-for i in range(len(m_lst)):
-    for j in n_lst:
-        if m_lst[i] == j:
-            cnt_lst[i] += 1
-            break
-print(*cnt_lst)
+
+n_lst.sort()
+
+def bin(arr, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+for i in range(m):
+    if bin(n_lst, m_lst[i], 0, n -1) is not None:
+        print(1, end = ' ')
+    else:
+        print(0, end = ' ')
